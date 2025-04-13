552646301f6f1cc673584fbc5b4245d1a1b7b50d
TuanAnh <n.tuananh221203@gmail.com>
2025-04-14 04:45:24 +0700

    Fix PORT environment variable

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ app.use(controller);
 app.set("views", __dirname + "/apps/views");
 app.set("view engine", "ejs");
 
-var server = app.listen(3000, function () {
-    console.log("Server is running http://localhost:3000");
+var server = app.listen(process.env.PORT || 3000, function () {
+    console.log("Server is running on port " + process.env.PORT || 3000);
 });
+
